ac0034d1b7f7caa9e968c601faa562f3ac60d537
TheRealArnav <tyagi.arnav02@gmail.com>
2025-05-25 14:05:47 +0200

    Add files via upload

---
+++ recylable.py
@@ -0,0 +1,93 @@
+import pygame
+from pygame.locals import*
+import random
+import time
+pygame.init()
+pygame.font.init()
+screen = pygame.display.set_mode((900,700))
+
+winorlose = "start"
+reyclablelist = ["C:/Pygame2/images/item1.png","C:/Pygame2/images/pencil.png","C:/Pygame2/images/box.png"]
+
+def bgchange(winorlose):
+    if winorlose == "start":
+        bg = pygame.image.load("C:/Pygame2/images/background,eco.png")
+        bg = pygame.transform.scale(bg,(900,700))
+        screen.blit(bg,(0,0))
+    
+class Bin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("C:/Pygame2/images/bin.png")
+        self.image = pygame.transform.scale(self.image,(35,50))
+        self.rect = self.image.get_rect()
+    def Draw(self):
+          
+        key =  pygame.key.get_pressed()
+        if key[K_UP]:
+            self.rect.y = self.rect.y - 5
+        if key[K_DOWN]:
+            self.rect.y = self.rect.y + 5
+        if key[K_RIGHT]:
+            self.rect.x = self.rect.x + 5
+        if key[K_LEFT]:
+            self.rect.x = self.rect.x - 5
+
+class recyclable(pygame.sprite.Sprite):
+    def __init__(self,img):
+        super().__init__()
+        self.image = pygame.image.load(img)
+        self.image = pygame.transform.scale(self.image,(25,40))
+        self.rect = self.image.get_rect()
+
+class nonrecyclable(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("C:/Pygame2/images/plasticbag.png")
+        self.image = pygame.transform.scale(self.image,(25,40))
+        self.rect = self.image.get_rect()
+
+
+bingroup = pygame.sprite.Group()
+bin = Bin()
+bingroup.add(bin)
+allsprites = pygame.sprite.Group()
+allsprites.add(bin)
+recylclablegroup = pygame.sprite.Group()
+nonrecyclablegroup = pygame.sprite.Group()
+
+for i in range(50):
+    item = recyclable(random.choice(reyclablelist))
+    item.rect.x = random.randint(0,900)
+    item.rect.y = random.randint(0,700)
+    allsprites.add(item)
+    recylclablegroup.add(item)
+
+for i in range(25):
+    item = nonrecyclable()
+    item.rect.x = random.randint(0,900)
+    item.rect.y = random.randint(0,900)
+    allsprites.add(item)
+    nonrecyclablegroup.add(item)
+
+
+
+
+run = True
+while run:
+    
+
+    bgchange(winorlose)
+    allsprites.draw(screen)
+    bin.Draw()
+
+
+    
+
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            quit()
+
+
+    pygame.display.flip()
